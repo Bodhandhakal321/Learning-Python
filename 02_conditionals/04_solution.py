93369cf48bd61ce1a46c8726de688367673f18db
Bodhan Dhakal <bodhandhakal22@gmail.com>
2024-02-14 09:33:29 +0545

    learned fruitripeness tester

---
+++ 02_conditionals/04_solution.py
@@ -0,0 +1,12 @@
+fruit = "banana"
+color = "Yellow"
+
+if fruit == "banana":
+    if color =="Green":
+        print("Unripe")
+    elif color == "Yellow":
+        print("ripe")
+    elif color == "Brown":
+        print("overripe")
+
+
