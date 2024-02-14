fcc87c86f98965973b8153d735faa8d99ee50cda
Bodhan Dhakal <bodhandhakal22@gmail.com>
2024-02-14 09:12:44 +0545

    age group categorization program using conditionals

---
+++ 02_conditionals/01_solution.py
@@ -0,0 +1,11 @@
+age = int(input("Provide me a age:"))
+if age <13:
+    print("User is Child")
+
+elif age < 20:
+    print("user is teenager")
+elif age <60:
+    print("USer is adult")
+else:
+    print("User is senior")
+
