44ff5133ebd40adac9620237ac37d2f4d33d943f
Bodhan Dhakal <bodhandhakal22@gmail.com>
2024-02-14 09:36:26 +0545

    weather activity suggestion

---
+++ 02_conditionals/05_solution.py
@@ -0,0 +1,10 @@
+weather = "Sunny"
+
+if weather == "Sunny":
+    activity = "go for walk"
+elif weather == "Rainy":
+    activity = "Read a book"
+elif weather == "Snowy":
+    activity = "build a snowman"
+
+print(activity)
