729eb34e8e901ccf0196ed8846f2bab3d7ffbb62
Bodhan Dhakal <bodhandhakal22@gmail.com>
2024-02-18 08:05:18 +0545

    inheritance creating electriccar class that inherits from the car class and has an additional attribute battery_size

--- 07_oop/01_solution.py
+++ 07_oop/01_solution.py
@@ -4,16 +4,29 @@ class Car:
         self.model = model
     def full_name(self):
         return f"{self.brand} {self.model}"
+    
+
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand,model)  
+        self.battery_size = battery_size
 
 
-my_car = Car("Toyota", "HILUX")
-print(my_car.brand)
-print(my_car.model)
-print(my_car.full_name())
+my_tesla = ElectricCar("Tesla", "Model S", "85KWH")
+print(my_tesla.model)
+print(my_tesla.full_name())
 
-my_new_car = Car("Tata","Safari")
-print(my_new_car.brand)
-print(my_new_car.model)
+
+
+# my_car = Car("Toyota", "HILUX")
+# print(my_car.brand)
+# print(my_car.model)
+# print(my_car.full_name())
+
+# my_new_car = Car("Tata","Safari")
+# print(my_new_car.brand)
+# print(my_new_car.model)
 
 # __init__ is a constructor 
 # addinng method to a class and displays the full name of class
+# inheritance creating electriccar class that inherits from the car class and has an additional attribute battery_size
